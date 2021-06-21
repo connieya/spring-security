efbd07d6e4065190c5de7472c1e2ff1fab692e29
gunny6026 <gunny6026@naver.com>
2021-06-21 20:58:24 +0900

    Add Spring Security dependency

---
+++ src/main/java/io/security/springboot/SpringController.java
@@ -0,0 +1,13 @@
+package io.security.springboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SpringController {
+
+    @GetMapping("/")
+    public String index(){
+        return "home";
+    }
+}
---
+++ src/test/java/io/security/springboot/SpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package io.security.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
